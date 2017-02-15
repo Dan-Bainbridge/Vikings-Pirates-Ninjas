6189cb1a08de4095ab9739324dbd24d1637e05df
Dan-Bainbridge <daniel.bainbridge@red-gate.com>
2017-02-15 14:46:04 +0000

    CreatedTestProjWithVikingTieTest

---
+++ VikingsPiratesNinjasTests/ProgramTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NinjaPirateViking;
+
+namespace VikingsPiratesNinjasTests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void TestForTieWhenBothPlayersHaveVikings()
+        {
+            //test both players have vikings = tie
+            Program.PlayGame();
+            Assert.AreEqual(1,Program.Tie);
+        }
+    }
+}
