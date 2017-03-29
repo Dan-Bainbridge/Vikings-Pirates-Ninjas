7bd6ca68d7ec8e2e4b4aee026d57eeb3ae3826c3
Dan-Bainbridge <daniel.bainbridge@red-gate.com>
2017-03-29 14:10:15 +0100

    Add counter tests

---
+++ VikingsPiratesNinjasTests/CounterTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VikingsPiratesNinjas;
+
+namespace VikingsPiratesNinjasTests
+{
+    [TestClass]
+    public class CounterTests
+    {
+        [TestMethod]
+        public void TestForNumberOfWinsIncreasingWithWinOutcome()
+        {
+            Counter gameCounter = new Counter();
+
+            gameCounter.AddResult(Outcome.Won);
+            Assert.AreEqual(1, gameCounter.GetWins());
+            Assert.AreEqual(0, gameCounter.GetDraws());
+            Assert.AreEqual(0, gameCounter.GetLoss());
+        }
+
+        [TestMethod]
+        public void TestForNumberOfDrawsIncreasingWithDrawOutcome()
+        {
+            Counter gameCounter = new Counter();
+
+            gameCounter.AddResult(Outcome.Drew);
+            Assert.AreEqual(1, gameCounter.GetDraws());
+        }
+    }
+}
