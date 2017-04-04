f36999395f9984f58bd03d36b1f1a7b8a20021f5
Dan-Bainbridge <daniel.bainbridge@red-gate.com>
2017-04-04 15:00:43 +0100

    Extracted Selection

--- paperScissorsRock/PlayGame.cs
+++ paperScissorsRock/PlayGame.cs
@@ -23,51 +23,6 @@ namespace VikingsPiratesNinjas
         public const string Pirate = "Pirate";
         public const string Exit = "Exit";
 
-        public Selection PlayerSelection()
-        {
-            string selection = Console.ReadLine();
-
-            switch (selection)
-            {
-                //Erm not sure about the case selects.
-                case Ninja:
-                    return Selection.Ninja;
-                case "n":
-                    return Selection.Ninja;
-                case Pirate:
-                    return Selection.Pirate;
-                case "p":
-                    return Selection.Pirate;
-                case Viking:
-                    return Selection.Viking;
-                case "v":
-                    return Selection.Viking;
-                case "e":
-                    return Selection.Exit;
-                case Exit:
-                    return Selection.Exit;
-                default:
-                    return Selection.Viking;
-            }
-        }
-
-        public Selection PcSelection()
-        {
-            Random rnd = new Random();
-            int choice = rnd.Next(1, 4);
-
-            switch (choice)
-            {
-                case 1:
-                    return Selection.Pirate;
-                case 2:
-                    return Selection.Ninja;
-                case 3:
-                    return Selection.Viking;
-                default:
-                    return Selection.Viking;
-            }
-        }
         public Outcome GameOutcome(Selection p1Selection, Selection p2Selection)
         {
 
--- paperScissorsRock/Program.cs
+++ paperScissorsRock/Program.cs
@@ -11,14 +11,18 @@ namespace VikingsPiratesNinjas
             Console.WriteLine("Please enter Viking(v)/Ninja(n)/Pirate(p) or Exit \r\n");
 
             PlayGame game = new PlayGame();
+            PlayerSelection playerSelection = new PlayerSelection();
 
             Console.WriteLine("Please make a selection:");
 
-            Selection p1Selection = game.PlayerSelection();
-            Selection p2Selection = game.PcSelection();
+            Selection p1Selection = playerSelection.UserSelection();
+            Selection p2Selection = playerSelection.PcSelection();
+
             Console.WriteLine("You have chosen: " + p1Selection);
             Console.WriteLine("The pc chose: " + p2Selection);
+
             Outcome outcome = game.GameOutcome(p1Selection, p2Selection);
+
             Console.WriteLine("You:" + outcome);
             gameCounter.AddResult(outcome);
             Console.ReadLine();
